524c1855a10e88d5a46d9074bcdbfe1419d1aecf
Văn Đình Hiếu <23120260@student.hcmus.edu.vn>
2025-03-08 00:21:46 +0700

    Update UX
    update message section and more box for each user in message box

---
+++ UX/homePageScript.js
@@ -0,0 +1,8 @@
+function showTable(id) {
+    var table = document.getElementById(id);
+    if (table.style.display === "none") {
+        table.style.display = "block"; // Hiển thị bảng
+    } else {
+        table.style.display = "none"; // Ẩn bảng
+    }
+}
